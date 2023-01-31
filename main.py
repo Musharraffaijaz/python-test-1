0355e24b78a99a6f41ebaad49f15b3aa93934852
Musharaf Aijaz <49266438+Musharraffaijaz@users.noreply.github.com>
2023-01-31 22:44:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -107,7 +107,83 @@ import math
 # else:
 #     print("Welcome User.")
 
+#compound intrest calculator
 
+# principle = 0
+# interest = 0
+# time = 0
+#
+# while principle <= 0:
+#     principle = float(input("Enter the principle amount:  "))
+#
+#     if principle <= 0 :
+#         print("Principle can't be less or equal to zero.")
+#
+# while interest <= 0:
+#     interest = float(input("Enter the interest amount:  "))
+#
+#     if interest <= 0 :
+#         print("interest can't be less or equal to zero.")
+#
+# while time <= 0:
+#     time = int(input("Enter the time period:  "))
+#
+#     if time <= 0 :
+#         print("Time can't be less or equal to zero.")
+#
+# finalAmount = principle * pow((1 + interest/100 ), time)
+# print(finalAmount)
 
+#time counter application
 
+# import time
+#
+# myTime = int(input("Enter your countdown time: "))
+#
+# for x in range(myTime, 0, -1):
+#     seconds = int(x % 60)
+#     minutes =  int(x / 60 )  % 60
+#     hours = int(x / 3600)
+#     print(f" {hours:02}:{minutes:02}:{seconds:02}")
+#     time.sleep(1)
+#
+# print("Times up!!!")
+
+#Pyton quiz time
+#
+# questions = ("Elements in a periodic table?: ",
+#              "Animal which lays the largest egg?: ",
+#              "Most abundant gas in earth atmosphere?: ",
+#              "Bones in a human body?: ",
+#              "Hottest planet in the solar system?: ")
+#
+# options = (("A.116", "B.117", "C.118", "D.119"),
+#            ("A.Whale", "B.Crocodile", "C.Elephant", "D.Ostrich"),
+#            ("A.NO2", "B.CO2", "C.O2", "D.SO2"),
+#            ("A.206", "B.207", "C.209", "D.208"),
+#            ("A.Mercury", "B.Venus", "C.Earth", "D.Mars"))
+#
+# answers = ("C", "D", "A", "A")
+#
+# guesses = []
+# score = 0
+# question_num = 0
+#
+# for question in questions:
+#     print("--------------------------------------")
+#     print(question)
+#     for option in options[question_num]:
+#         print(option)
+#
+#
+#
+#
+#     guesses = input("Enter an option (A, B, C, D)").upper()
+#     guesses.append(guesses)
+#     if guesses == answers[question_num]:
+#         print("Your answer is correct!")
+#         score += 1
+#     else:
+#         print("The answer is incorrect!")
+#     question_num += 1
 
